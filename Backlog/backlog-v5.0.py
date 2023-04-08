107515e9b29d687e85c482eb021d022caada5d65
A89881 <abhay.sanatkumarmishra@elev.ga.ntig.se>
2023-04-09 00:39:03 +0200

    progress
    slow progress, working on the scoreboard still

--- Backlog/backlog-v5.0.py
+++ Backlog/backlog-v5.0.py
@@ -12,9 +12,9 @@ class Wordle:
         self.max_attempts = 6
         self.word_length = 5
         self.board_list = []
+        self.dict1 = {}
 
-        self.scoreboard_dict = {}
-        self.name_list = []
+
 
 
     def find_words(self):
@@ -27,34 +27,12 @@ class Wordle:
         self.secret_word = [str(i) for i in self.secret_word]
     
     def user(self):
-        print(" ")
-        print("If you want to save your score Enter Username Below, else Enter 'C' to continue (Be Warned Score Will not be Saved) ")
-        username = str(input("Enter Username? (Max 5 Characters): "))
-        check = [str(x).lower() for x in username]
-        c = ["c"]
+        print("If you want to save ")
+        username = str(input("Enter Username?: "))
 
-        while True:
-            if len(check) <= 5:
-                if check == c:
-                    break
-                else:
-                    self.name_list.append(username)
-                    break
-            else: 
-                print("Invalid Username; too long")
-                print("If you want to save your score Enter Username Below, else Enter 'C' to continue ")
-                username = str(input("Enter Username? (Max 5 Character): ")).lower()
-                check = [str(x) for x in username]
+        pass
 
-            
-
-
-    def Scoreboard(self):
-        self.scoreboard_dict = {item: None for item in self.name_list}
-        os.system("cls")
-        # print("The Scoreboard is made of the Top 100 Players, so if your name isn't there, sorry to say it to you but your trash")
-        print("The Scoreboared ranks the players based on Amount of Guesses required for them to win and the amount of time it took to win")
-        print(self.scoreboard_dict)
+    def printScoreboard(self):
         pass
     
 
@@ -67,8 +45,7 @@ class Wordle:
     def play_game(self):
         main_loop = True
         counter = 1
-        start_time = time.time()
-        player_record = []
+        #  start_time = time.time()
 
         while main_loop:
           if counter < self.max_attempts + 1:
@@ -88,28 +65,16 @@ class Wordle:
                             for i in range(0, inputed_word_length):
                                 if inputed_word[i] == self.secret_word[i]:
                                     inputed_word[i] = colored(inputed_word[i], "green")   
-
-                            
-                            end_time = time.time()
-                            total_time = round(end_time - start_time, 3) 
+          
+                         
                             print("You Won!")
                             inputed_word = "".join(map(str, inputed_word))
                             self.board_list.append(inputed_word)
                             self.board()
                             print(f"The Word is {inputed_word}")
                             print(f"It took you {counter} attempt(s)")
-                            print(f"It took you in total {total_time} seconds")
-                            
-                            player_record.append(counter)
-                            player_record.append(total_time)
-                            print(player_record)
-
-                            self.user()
-
-                            
-
-                            # self.scoreboard_dict.update({self.name: player_record})
-                            # self.scoreboard_dict[self.name] = player_record
+                            # end_time = time.time()
+                            # print(f"It took you in total {total_time} seconds")
                             continue_start()
                             break
                             
@@ -118,11 +83,14 @@ class Wordle:
                             for i in range(0, inputed_word_length):
                                 if inputed_word[i] == self.secret_word[i]: 
                                     inputed_word[i] = colored(inputed_word[i], "green")  
+                           
                                 elif inputed_word[i] in self.secret_word:
                                     inputed_word[i] = colored(inputed_word[i], "yellow")
+                                
                                 else:
                                     pass
                             
+        
                             self.board_list.append(inputed_word)
                             counter += 1                 
                 else:
@@ -152,7 +120,7 @@ def start():
             main()
             break
         elif play == "s":
-            wordle.Scoreboard()
+            wordle.printScoreboard()
             play = str(input("Enter 'P' if you wish to Play, Enter 'S' to access Scoreboard or Enter 'E' to Exit?: ")).lower()
         elif play == "e":
             break
@@ -170,7 +138,7 @@ def continue_start():
             main()
             break
         elif play == "s":
-            wordle.Scoreboard()
+            wordle.printScoreboard()
             play = str(input("Enter 'P' if you wish to Play, Enter 'S' to access Scoreboard or Enter 'E' to Exit?: ")).lower()
         elif play == "e":
             break
@@ -180,6 +148,6 @@ def continue_start():
             play = str(input("Enter 'P' if you wish to Play, Enter 'S' to access Scoreboard or Enter 'E' to Exit?: ")).lower()
 
 
-
+      
 if __name__ == "__main__":
     start()
--- Extra/test.py
+++ Extra/test.py
@@ -1,142 +1,3 @@
-import os
-import re
-import sys
-import time
-from termcolor import colored
-
-
-class main:
-    def __init__(self):
-        self.chosenWord = "horse"
-        self.board = [
-            "     ",
-            "     ",
-            "     ",
-            "     ",
-            "     ",
-        ]
-        self.activeRow = 0
-        pass
-
-    def welcome(self):
-        print("Welcome to Wordle\n")
-        print("██╗    ██╗ ██████╗ ██████╗ ██████╗ ██╗     ███████╗")
-        print("██║    ██║██╔═══██╗██╔══██╗██╔══██╗██║     ██╔════╝")
-        print("██║ █╗ ██║██║   ██║██████╔╝██║  ██║██║     █████╗  ")
-        print("██║███╗██║██║   ██║██╔══██╗██║  ██║██║     ██╔══╝  ")
-        print("╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝███████╗███████╗")
-        print(" ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚══════╝")
-        print("\n")
-
-    def printRow(self, row):
-        sys.stdout.write("              ")  # to center the board
-        for i in range(len(row)):
-            if row != "":
-                if row[i] == self.chosenWord[i]:
-                    sys.stdout.write(
-                        f'| {self.correctPlace(row[i].upper())} ')
-                    pass
-                elif row[i] in self.chosenWord:
-                    sys.stdout.write(
-                        f'| {self.correctLetter(row[i].upper())} ')
-                    pass
-                else:
-                    sys.stdout.write(
-                        f'| {self.wrongLetter(row[i].upper())} ')
-                    pass
-
-        sys.stdout.write("|\n\n")
-        pass
-
-    def printBoard(self):
-        for row in self.board:
-            self.printRow(row)
-            pass
-
-        sys.stdout.write("\n")
-        pass
-
-    def correctPlace(self, string):
-        return colored(string, "green")
-
-    def correctLetter(self, string):
-        return colored(string, "yellow")
-
-    def wrongLetter(self, string):
-        return colored(string, "red")
-
-    def defineNewWord(self):
-        # do stuff with textHandler
-        # except old words so you dont get duplicates, thats no fun
-        pass
-
-    def checkInputWord(self):
-        def sliceWord(reasign, i):
-            slicedRow = []
-            for letter in self.board[self.activeRow]:
-                slicedRow.append(letter)
-
-            slicedRow[i] = reasign
-            self.board[self.activeRow] = "".join(slicedRow)
-
-        response = input("... ").lower()
-        response = re.sub(r'[^a-z]', "", response)
-
-        # first check if it's actually five letters or not
-        if len(response) != 5:
-            print("Only five letter words please.\n")
-            time.sleep(.8)
-            return
-        pass
-
-        # secondly for each letter in the guess we'll check if they match with the chosen word's letter
-        for i in range(len(response)):
-            if response[i] == self.chosenWord[i]:
-                sliceWord(self.chosenWord[i], i)
-                pass
-            elif response[i] in self.chosenWord:
-                sliceWord(response[i], i)
-                pass
-            else:
-                sliceWord(response[i], i)
-                pass
-            pass
-
-        # self.printRow(self.board[self.activeRow])
-
-        if response == self.chosenWord:
-            print("You guessed the word.")
-            raise SystemExit
-
-        if self.activeRow < len(self.board) - 1:
-            self.activeRow += 1
-        else:
-            print("game over")
-            raise SystemExit
-
-    def run(self):
-        # this loops
-
-        # clear terminal
-        def clear():
-            os.system("cls")
-            sys.stdout.write("\n")
-
-        clear()
-
-        self.welcome()
-        self.printBoard()
-        self.checkInputWord()
-
-        self.run()
-
-
-mainClass = main()
-
-mainClass.run()
-
-
-
 """
 Shit
 """
@@ -175,3 +36,39 @@ Shit
 
 # test = Test()                  
 # test.board()
+
+# class Test:
+#     def __init__(self):
+#         self.dict1 = {}
+#         pass
+    
+# test = Test()
+# print(test.dict1)
+
+# my_dictionary = {}
+# my_list = [["three", 3], ["four", 4]]
+# print("Before:", my_dictionary)
+# for key,value in my_list:
+#     my_dictionary[key] = value
+# print("After:", my_dictionary)
+
+# list1 = ["1", "2"]
+
+# # Using update() method
+# my_dict = {}
+# my_dict.update({"my_list": list1})
+# print(my_dict)  # Output: {'my_list': ['1', '2']}
+
+# # Using square bracket notation
+# my_dict = {}
+# my_dict["my_list"] = list1
+# print(my_dict)  # Output: {'my_list': ['1', '2']}
+
+my_list = ['apple', 'banana', 'orange']
+my_dict = {item: None for item in my_list}
+print(my_dict)
+
+my_list.append("berry")
+print(my_list)
+print(my_dict)
+
